97385f7ef7a2df412e76c909b3381fa438eef559
avinash singh <avinash.singh54@gmail.com>
2023-07-16 18:02:50 +0530

    Adding beer-service local profile for Mysql

--- src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
+++ src/main/java/guru/sfg/brewery/configserver/MsscConfigServerApplication.java
@@ -2,8 +2,10 @@ package guru.sfg.brewery.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MsscConfigServerApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class MsscConfigServerApplication {
 	}
 
 }
+//https://www.udemy.com/course/spring-boot-microservices-with-spring-cloud-beginner-to-guru/learn/lecture/18588296#overview
